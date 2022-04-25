76f95afe81bc1a689eb20df3abe7deada255d326
Jowel Castaneda <jowelcastaneda6@gmail.com>
2022-04-26 04:14:55 +0800

    remove thumbnail

--- pages/index.js
+++ pages/index.js
@@ -16,10 +16,7 @@ import Paragraph from '../components/paragraph'
 import { BioSection, BioYear } from '../components/bio'
 import Layout from '../components/layouts/article'
 import Section from '../components/section'
-import { GridItem } from '../components/grid-item'
 import { IoLogoTwitter, IoLogoInstagram, IoLogoGithub, IoLogoFacebook } from 'react-icons/io5'
-import thumbYouTube from '../public/images/links/jowel.png'
-import thumbInkdrop from '../public/images/works/linkedin.png'
 import Image from 'next/image'
 
 const ProfileImage = chakra(Image, {
@@ -178,23 +175,6 @@ const Home = () => (
             </Link>
           </ListItem>
         </List>
-
-        <SimpleGrid columns={[1, 2, 2]} gap={6}>
-          <GridItem
-            href="https://www.youtube.com/watch?v=O7E4hOkfLWA&t=88s"
-            title="Dev as Life"
-            thumbnail={thumbYouTube}
-          >
-            My YouTube channel
-          </GridItem>
-          <GridItem
-            href="https://www.linkedin.com/in/jowel-casta%C3%B1eda-667659158/"
-            title="Linkedin"
-            thumbnail={thumbInkdrop}
-          >
-            A Markdown note-taking app
-          </GridItem>
-        </SimpleGrid>
 
         <Box align="center" my={4}>
           <NextLink href="/posts" scroll={false}>
